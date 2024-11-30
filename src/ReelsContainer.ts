965e28b479fbef244ee0814db4e3cd23cf7bf14f
DTomArt <dtomart@gmail.com>
2024-11-30 16:33:49 +0100

    delete redundant masks and backgroundContainer

--- src/ReelsContainer.ts
+++ src/ReelsContainer.ts
@@ -1,6 +1,5 @@
-import { Container, Graphics, Texture } from "pixi.js";
+import { Container, Texture } from "pixi.js";
 import { Reel } from "./types.js";
-import { REEL_WIDTH, SYMBOL_SIZE } from "./main.js";
 import { ReelContainer } from "./ReelContainer.js";
 
 export class ReelsContainer extends Container {
@@ -22,21 +21,6 @@ export class ReelsContainer extends Container {
       [1, 1, 1, 2, 2, 0, 2, 3, 3, 3, 3, 1, 2, 2, 0],
       [3, 1, 2, 3, 2, 3, 3, 2],
     ];
-
-    const mask = new Graphics()
-      .beginFill(0xff0000)
-      .drawRect(0, 0, REEL_WIDTH * 5, SYMBOL_SIZE * 6)
-      .endFill();
-    // this.mask = mask;
-
-    const backgroundContainer = new Container();
-    this.addChild(backgroundContainer);
-
-    const symbolsMask = new Graphics()
-      .beginFill(0xff0000)
-      .drawRect(0, 0, REEL_WIDTH * 5, SYMBOL_SIZE * 5)
-      .endFill();
-    // backgroundContainer.mask = symbolsMask;
 
     for (let i = 0; i < 3; i++) {
       const reelContainer = new ReelContainer(i, reelStrips[i], slotTextures);
